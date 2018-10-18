905d8aa1220790990ff1addd75841b23bac323be
Crosty <Stanie_cheung@msn.com>
2018-10-18 23:34:09 +0200

    Added Interface

---
+++ HandIn 1/RouletteGame/RouletteGame/Interface/IStartGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RouletteGame.Interface
+{
+    public interface IStartGame
+    {
+        void Play();
+        int RandomizeNumber { get; set; }
+        string RandomizeColor { get; set; }
+    }
+}
--- HandIn 1/RouletteGame/RouletteGame/Models/StartGame.cs
+++ HandIn 1/RouletteGame/RouletteGame/Models/StartGame.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using RouletteGame.Interface;
 
 namespace RouletteGame.Models
 {
-    public class StartGame
+    public class StartGame : IStartGame
     {
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RandomizeNumber { get; set; }
+        public string RandomizeColor { get; set; }
     }
 }
