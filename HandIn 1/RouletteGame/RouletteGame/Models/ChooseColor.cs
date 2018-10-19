c83a15267f4a3c1d1b2f1deec47c362df039262e
Crosty <Stanie_cheung@msn.com>
2018-10-19 11:21:50 +0200

    Added Models and Interfaces

---
+++ HandIn 1/RouletteGame/RouletteGame/Models/ChooseColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RouletteGame.Interface;
+
+namespace RouletteGame.Models
+{
+    public class ChooseColor : IChooseColor
+    {
+        public string SpecificColor(string c)
+        {
+            var Color_Black = 0;
+            var Color_Red = 1;
+            //var Color_Green = 2;
+
+            //If user chooses red, select "1" and return to "spinwheel"
+
+            return 0.ToString();
+        }
+    }
+}
